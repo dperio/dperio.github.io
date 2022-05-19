528623fb1c3fdaa1d6415d557e7a76c4b1c17f0d
dperio <daquanperio@gmail.com>
2022-05-19 18:14:35 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,23 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas, 100, 30);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 8 : Call the drawCircle() function 
-
-
+        for (var loopsDone = 0; loopsDone < 101; loopsDone++){
+            drawCircle();
+        }
+       
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,14 +48,23 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
             
+            
+             // i deleted this code  because when we use iteration the code that was here will run multiple times
+            
+            
+            
+            
+            
+            for(var eachCircle = 0; eachCircle < 101; eachCircle++){
+                physikz.updatePosition(circles[eachCircle]);
+                game.checkCirclePosition(circles[eachCircle]);
+            }
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+            //i deleted this code  because when we use iteration the code that was here will run multiple times
             // TODO 9 : Iterate over the array
-           
-            
+         
+          
         }
     
         /* 
@@ -62,7 +80,19 @@ var init = function (window) {
             }
             
             // TODO 7 : YOUR CODE STARTS HERE //////////////////////
-            
+           // if the circle has gone to high on the screen then place it down on the screen
+            else if (circle.y > canvas.height) {
+                circle.y = 0;
+            } 
+            //if the circle has gone to the bottom of the screen then place it back at the top
+            else if (circle.y < 0) {
+                circle.y = canvas.height;
+            } 
+            //if the cirle has gone to far to the left then place the cirlce on the right of the screen
+            else if (circle.x < 0) {
+                circle.y = canvas.width;
+            }
+           
 
 
             // YOUR TODO 7 CODE ENDS HERE //////////////////////////
